03d2238c1715edc92116458916c1b88bd3369c74
Nicolas Wagner <contact@wagner-nicolas.com>
2017-11-29 09:34:25 -0500

    [RMV] remove crowdjury wording

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'Crowdjury - Kleros',
+  community: process.env.COMMUNITY_NAME || 'Kleros',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'kleros.slack.com',
   // access token of slack
